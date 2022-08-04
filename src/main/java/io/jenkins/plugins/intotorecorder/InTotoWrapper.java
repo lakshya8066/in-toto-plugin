584ae58c7e7b54b8a936280ab2a3eb7c44c3923f
Lakshya Gupta <lakshya.gupta.1999@gmail.com>
2022-08-04 18:11:49 +0530

    some work

--- src/main/java/io/jenkins/plugins/intotorecorder/InTotoWrapper.java
+++ src/main/java/io/jenkins/plugins/intotorecorder/InTotoWrapper.java
@@ -8,10 +8,17 @@ import io.github.intoto.legacy.keys.RSAKey;
 import io.github.intoto.legacy.models.Artifact;
 import io.github.intoto.legacy.models.Link;
 import io.github.intoto.legacy.models.Artifact.ArtifactHash;
+import io.github.intoto.slsa.models.v02.ConfigSource;
+import io.github.intoto.slsa.models.v02.Invocation;
+import io.github.intoto.slsa.models.v02.Material;
+import io.github.intoto.slsa.models.v02.Metadata;
+import io.github.intoto.slsa.models.v02.Provenance;
 import io.jenkins.plugins.intotorecorder.transport.Transport;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
 import hudson.Extension;
@@ -38,7 +45,9 @@ import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.StaplerRequest;
-
+import org.apache.commons.compress.archivers.Lister;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.util.encoders.Hex;
 import org.jenkinsci.Symbol;
 
 import java.io.File;
@@ -48,12 +57,18 @@ import java.io.OutputStreamWriter;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.HashMap;
-import java.util.Set;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Collections;
 import java.lang.InterruptedException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -122,6 +137,10 @@ public class InTotoWrapper extends SimpleBuildWrapper {
      */
     private FilePath cwd;
 
+    Provenance provenance;
+
+    ConfigSource configSource;
+
     @DataBoundConstructor
     public InTotoWrapper(String credentialId, String keyPath, String stepName, String transport)
     {
@@ -170,7 +189,13 @@ public class InTotoWrapper extends SimpleBuildWrapper {
                                EnvVars initialEnvironment)
                         throws IOException,
                                InterruptedException {
-        this.cwd = workspace;
+        this.cwd = workspace;   
+        listener.getLogger().println("getDescription():  "+launcher.getComputer().getDescription());
+        listener.getLogger().println("getName():  "+launcher.getComputer().getName());
+        listener.getLogger().println("getUrl():  "+launcher.getComputer().getUrl());
+        listener.getLogger().println("getEnvironment():  "+launcher.getComputer().getEnvironment());
+        listener.getLogger().println("getWorkspaceList():  "+launcher.getComputer().getWorkspaceList().toString());
+        listener.getLogger().println("getBuilds:  "+launcher.getComputer().getBuilds());
 
         listener.getLogger().println("[in-toto] wrapping step ");
         listener.getLogger().println("[in-toto] using step name: " + this.stepName);
@@ -186,11 +211,165 @@ public class InTotoWrapper extends SimpleBuildWrapper {
         this.link = new Link(null, null, this.stepName, null, null, null);
         this.link.setMaterials(InTotoWrapper.collectArtifacts(this.cwd));
 
-        listener.getLogger().println("[in-toto] Dumping metadata... ");
+        //Setting up Provenance object
+        this.provenance=new Provenance();
 
-        context.setDisposer(new PostWrap(this.link, this.key, this.keyPath, this.stepName,
-                    this.transport));
+        //Setting up materials
+        setProvenanceMaterials(InTotoWrapper.collectArtifacts(this.cwd));
+
+        // //Setting up invocation
+        // listener.getLogger().println(build.ARTIFACTS);
+        // listener.getLogger().println(build.FEED_ADAPTER_LATEST);
+        // listener.getLogger().println(build.getBuildStatusUrl());
+        // listener.getLogger().println(build.getExternalizableId());
+        // listener.getLogger().println(build.getSearchUrl());
+        // listener.getLogger().println(build.getApi().bean);
+        // listener.getLogger().println(build.getArtifactManager());
+        // listener.getLogger().println(build.getBuildStatusSummary().message);
+        // //listener.getLogger().println(build.getCauses());
+        // listener.getLogger().println(build.getEnvironment(listener));
+        
+        // listener.getLogger().println(build.getParent().getAllProperties());
+        // listener.getLogger().println(build.getParent().getCharacteristicEnvVars());
+        // listener.getLogger().println(build.getParent().getDescription());
+
+        BuildInvocation invocation=new BuildInvocation();
+        invocation.setEnvironments(initialEnvironment);
+        try {
+            invocation.setConfigSource(initialEnvironment);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        provenance.setInvocation(invocation);
+        DateFormat obj = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");   
+        Date start=new Date(build.getStartTimeInMillis());
+        Date end=new Date(build.getStartTimeInMillis()+build.getDuration());
+        listener.getLogger().println(start);
+        listener.getLogger().println(end);
+
+        // listener.getLogger().println(invocation.getConfigSource().getDigest());
+        // listener.getLogger().println(initialEnvironment.get("GIT_PREVIOUS_SUCCESSFUL_COMMIT"));
+
+        //listener.getLogger().println(initialEnvironment.masterEnvVars);
+        
+
+        //listener.getLogger().println(invocation.environment);
+
+
+        //listener.getLogger().println(configSource.getUri()+"       "+configSource.getDigest());
+            
+
+        //listener.getLogger().println("initialEnvironment:\n"+initialEnvironment);
+
+        //PRINTING THE PROVENANCE
+
+        // for (Material material:provenance.getMaterials()){
+        //     listener.getLogger().println("URI : "+material.getUri());
+        //     listener.getLogger().println("Digest : " +material.getDigest());
+        //     listener.getLogger().println();
+        // }
+
+        // listener.getLogger().println("[in-toto] Dumping metadata... ");
+
+        // context.setDisposer(new PostWrap(this.link, this.key, this.keyPath, this.stepName,
+        //             this.transport));
     }
+    private class BuildInvocation extends Invocation{
+        public Map<String,String> environment;
+
+        public Object parameters;
+
+        public void setEnvironments(EnvVars initialEnvironment){
+            environment=initialEnvironment;
+            environment.remove("HUDSON_SERVER_COOKIE");
+            environment.remove("JENKINS_SERVER_COOKIE");
+        }
+
+        public void setConfigSource(EnvVars initialEnvironment) throws NoSuchAlgorithmException{
+        
+            ConfigSource configSource=new ConfigSource();
+            String git_url=initialEnvironment.get("GIT_URL");
+            configSource.setUri(git_url);
+
+            String lastCommit=initialEnvironment.get("GIT_PREVIOUS_SUCCESSFUL_COMMIT");
+       
+            // One way to get sha256, but it gives extra bytes
+            // SHA256Digest digest = new SHA256Digest();
+            // byte[] result=lastCommit.getBytes();
+            // digest.update(result,0,result.length);
+            // digest.doFinal(result, 0);
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(
+            lastCommit.getBytes(StandardCharsets.UTF_8));
+            String result = new String(Hex.encode(hash));
+
+            Map<String,String> uriDigestMap=new HashMap<String,String>();
+            uriDigestMap.put("sha256", result);
+            configSource.setDigest(uriDigestMap);
+            super.setConfigSource(configSource);
+        }
+
+    }
+    
+
+    private void setProvenanceMaterials(HashMap<String, ArtifactHash> results) {
+        HashMap<String, ArtifactHash> materials =new HashMap<String, ArtifactHash>();
+        materials=link.excludeArtifactsByPattern(results,null);
+        Iterator hmIterator = materials.entrySet().iterator();
+        List<Material> provenanceMaterials= new ArrayList<Material>();
+        while (hmIterator.hasNext()) {
+            Material material=new Material();
+            Map.Entry<String, ArtifactHash> new_Map = (Map.Entry<String, ArtifactHash>)hmIterator.next();
+            material.setUri(new_Map.getKey());
+            material.setDigest(new_Map.getValue());
+            provenanceMaterials.add(material);
+        }
+        provenance.setMaterials(provenanceMaterials);
+    }
+//    transient String defaultExcludePattern = "**.{git,link}**";
+//    public HashMap<String, ArtifactHash> excludeArtifactsByPattern(
+//        HashMap<String, ArtifactHash> materials, String pattern) {
+//        String patternString;
+//        HashMap<String, ArtifactHash> filtered_artifacts;
+//
+//        if (pattern != null && pattern.length() != 0) {
+//            patternString = pattern;
+//        } else {
+//            patternString = defaultExcludePattern;
+//        }
+//
+//        FileSystem fileSystem = FileSystems.getDefault();
+//
+//        PathMatcher pathMatcher = fileSystem.getPathMatcher("glob:" + patternString);
+//
+//        filtered_artifacts = materials;
+//
+//        Iterator<HashMap.Entry<String, ArtifactHash>> iterator =
+//            filtered_artifacts.entrySet().iterator();
+//
+//        while (iterator.hasNext()) {
+//
+//            HashMap.Entry<String, ArtifactHash> entry = iterator.next();
+//
+//            if (pathMatcher.matches(Paths.get(entry.getKey()))) {
+//                iterator.remove();
+//            }
+//        }
+//
+//        return filtered_artifacts;
+//    }
+
+    private void setMetadata(Run<?,?> build){
+        Metadata metadata=new Metadata();
+        DateFormat Date = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");   
+        Date start=new Date(build.getStartTimeInMillis());
+        Date end=new Date(build.getStartTimeInMillis()+build.getDuration());
+        // metadata.setBuildStartedOn(start);
+        // metadata.setBuildFinishedOn(end);
+    }
+
 
     private void loadKey(Reader reader) {
         this.key = RSAKey.readPemBuffer(reader);
